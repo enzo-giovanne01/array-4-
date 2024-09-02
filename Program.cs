2ea5f8b94d4ebbf94e6132e2a8dff17785cdab8f
enzo-giovanne01 <giovanneenzo01@gmail.com>
2024-09-01 23:37:26 -0300

    array-4-

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+
+int[]numeros = new int[10];
+int[]pares = new int[10];
+int[]impares = new int[10];
+for (int i = 0; i < numeros.Length; i++)
+{
+    WriteLine("Digite um número maior que zero.");
+    numeros[i] = Convert.ToInt32(ReadLine());
+}
+for (int p = 0; p < numeros.Length; p++)
+{
+    if (numeros[p] %2 == 0)
+    {
+        pares[p] = numeros[p];
+    }
+    else
+    {
+        impares[p] = numeros[p];
+    }
+}
+Array.Sort(numeros);
+Array.Sort(pares);
+Array.Sort(impares);
+Clear();
+WriteLine("Números digitados.... ");
+foreach(int item in numeros)
+{
+    WriteLine(item);
+}
+WriteLine("Números pares... ");
+foreach(int item in pares)
+{
+    WriteLine(item);
+}
+WriteLine("Números impares... ");
+foreach(int item in impares)
+{
+    WriteLine(item);
+}
+
